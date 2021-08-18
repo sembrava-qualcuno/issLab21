f4ba985e0f73f273b3619878befd03732559d05d
m4rkdc <gianmarco.decola97@gmail.com>
2021-08-18 14:18:49 +0200

    :white_check_mark: Complete product owner test

--- it.unibo.sembrava_qualcuno.sprint4/parkmanagerservice/src/test/kotlin/parkmanagerservice/Sprint4FinalTest.kt
+++ it.unibo.sembrava_qualcuno.sprint4/parkmanagerservice/src/test/kotlin/parkmanagerservice/Sprint4FinalTest.kt
@@ -1,20 +1,20 @@
 package parkmanagerservice
 
+import ch.qos.logback.core.util.OptionHelper.getEnv
 import connQak.connQakBase
 import connQak.connQakTcp
 import fan.FanMock
 import it.unibo.kactor.ActorBasic
 import it.unibo.kactor.QakContext
-import kotlinx.coroutines.GlobalScope
+import itunibo.planner.plannerUtil
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.eclipse.californium.core.CoapResource
 import org.eclipse.californium.core.CoapServer
+import org.junit.Assert
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -122,34 +122,61 @@ class Sprint4FinalTest {
         }
     }
 
+    @OptIn(ObsoleteCoroutinesApi::class)
     @Test
-    @Tag("NoObserve")
     fun productOwnerTest() {
         ParkingAreaKb.slot[1] = "117082021120000"
         ParkingAreaKb.slot[3] = "317082021130000"
         ParkingAreaKb.slot[5] = "517082021140000"
+        val channelForObserver = Channel<String>()
+        testingObserver!!.addObserver(channelForObserver)
+       runBlocking {
+           launch(newSingleThreadContext("SpringThread")) {
+               mockMvc.perform(MockMvcRequestBuilders.get("/client/reqenter")).andDo(MockMvcResultHandlers.print()).andExpect(
+                       MockMvcResultMatchers.status().isOk)
+                       .andExpect(MockMvcResultMatchers.content().json(Json.encodeToString(ParkingSlot(2))))
 
-        //Send reqenter
-        mockMvc.perform(MockMvcRequestBuilders.get("/client/reqenter")).andDo(MockMvcResultHandlers.print()).andExpect(
-            MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.content().json(Json.encodeToString(ParkingSlot(2))))
+               delay(2000)
+               weightSensor.updateResource(1000)
 
-        runBlocking { delay(2000) }
-        weightSensor.updateResource(1000)
+               //Send carenter
+               val result =
+                       mockMvc.perform(MockMvcRequestBuilders.get("/client/carenter?slotnum=2")).andDo(MockMvcResultHandlers.print()).andExpect(
+                               MockMvcResultMatchers.status().isOk).andReturn()
 
-        //Send carenter
-        val result =
-            mockMvc.perform(MockMvcRequestBuilders.get("/client/carenter?slotnum=2")).andDo(MockMvcResultHandlers.print()).andExpect(
-                MockMvcResultMatchers.status().isOk).andReturn()
-        val tokenId: TokenId = Json.decodeFromString(result.response.contentAsString)
+               weightSensor.updateResource(0)
+               delay(2000)
 
-        weightSensor.updateResource(0)
-        runBlocking { delay(2000) }
+               val tokenId = TokenId("117082021120000")
+               //Send reqexit
+               mockMvc.perform(MockMvcRequestBuilders.get("/client/reqexit?tokenid=${tokenId.tokenId}")).andDo(MockMvcResultHandlers.print()).andExpect(
+                       MockMvcResultMatchers.status().isOk)
+           }
+           launch {
+               Assert.assertEquals("trolley at HOME", channelForObserver.receive())
+               Assert.assertEquals("trolley trip to INDOOR start", channelForObserver.receive())
+               Assert.assertEquals("trolley trip to INDOOR end", channelForObserver.receive())
 
-        //Send reqexit
-        mockMvc.perform(MockMvcRequestBuilders.get("/client/reqexit?tokenid=${tokenId.tokenId}")).andDo(MockMvcResultHandlers.print()).andExpect(
-            MockMvcResultMatchers.status().isOk)
+               Assert.assertTrue(plannerUtil.atPos(6, 0))
 
-        runBlocking { delay(15000) }
+               Assert.assertEquals("trolley moveToPark(2)", channelForObserver.receive())
+               Assert.assertEquals("trolley trip to park slot 2 end", channelForObserver.receive())
+
+               Assert.assertTrue(plannerUtil.atPos(1, 2))
+
+               Assert.assertEquals("trolley moveToPark(1)", channelForObserver.receive())
+               Assert.assertEquals("trolley trip to park slot 1 end", channelForObserver.receive())
+
+               Assert.assertTrue(plannerUtil.atPos(1, 1))
+
+               Assert.assertEquals("trolley trip to OUTDOOR start", channelForObserver.receive())
+               Assert.assertEquals("trolley trip to OUTDOOR end", channelForObserver.receive())
+
+               Assert.assertTrue(plannerUtil.atPos(6, 4))
+
+               Assert.assertEquals("trolley IDLE", channelForObserver.receive())
+               Assert.assertEquals("trolley at HOME", channelForObserver.receive())
+           }
+       }
     }
 }
---
+++ it.unibo.sembrava_qualcuno.sprint4/parkmanagerservice/src/test/resources/carparking.pl
@@ -0,0 +1,10 @@
+%====================================================================================
+% carparking description   
+%====================================================================================
+context(ctxparkmanagerservice, "localhost",  "TCP", "8023").
+context(ctxtrolley, "localhost",  "TCP", "8024").
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( clientservice, ctxparkmanagerservice, "clientservice.Clientservice").
+  qactor( managerservice, ctxparkmanagerservice, "managerservice.Managerservice").
+  qactor( trolley, ctxtrolley, "trolley.Trolley").
